b2f88ae156101c1116618b46d44c39c06c4def58
Liusk-dev <128986601+Liusk-dev@users.noreply.github.com>
2024-08-08 11:03:39 +0800

    Update Card.js

--- js/Card.js
+++ js/Card.js
@@ -4,12 +4,7 @@ var audio = document.getElementById("audio");
 let door = document.getElementById("door");
 let door1 = document.getElementById("door1");
 
-let up = document.getElementById("up");
-let down = document.getElementById("down");
-
 card.addEventListener("click", function () {
-    up.style.display = "none";
-    down.style.display = "none";
     card.style.display = "none";
     door.classList.toggle("active");
     door1.classList.toggle("active");
